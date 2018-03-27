cc4cb8b3068e8035233d97fa10731fb63002c6ff
Archonsh <archonshen@gmail.com>
2018-03-27 19:04:58 +0800

    fix cal distance

--- django/schools/views.py
+++ django/schools/views.py
@@ -107,8 +107,8 @@ def distant_selection(target_distant, zip, collection):
 
 def calculatedistance(zip_home, zip_school):
     query = "https://maps.googleapis.com/maps/api/directions/json?origin=home&destination=school&region=sg&key=AIzaSyALTRUtyRv0xcAxEj1mJklVHHXnU77OVE4"
-    query.replace("home", zip_home)
-    query.replace("school", zip_school)
+    query.replace("home", str(zip_home))
+    query.replace("school", str(zip_school))
     with urllib.request.urlopen(query) as url:
         data = json.loads(url.read().decode())
         dist = data['routes'][0]['legs'][0]['distance']['value']
