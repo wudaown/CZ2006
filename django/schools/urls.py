d684dacc3e0490c8dfa9040233c18883acbd9419
LISH0038 <melodyli710@gmail.com>
2018-03-27 14:19:06 +0800

    url

--- django/schools/urls.py
+++ django/schools/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SchoolListView,SchoolDetailView,saveToList,guild_search,advanced_search
+from .views import SchoolListView,SchoolDetailView,saveToList,guild_search,advanced_search,deleteFromList
 
 urlpatterns = [
 	path('', guild_search, name='school'),
@@ -7,5 +7,6 @@ urlpatterns = [
     path('advance/', advanced_search, name='detail'),
 	path('', SchoolListView.as_view(), name='kindergarten-list'),
     path('<int:pk>/', SchoolDetailView.as_view(), name='school-detail'),
-    path(r'^(?P<pk>)/$',saveToList,name ='saveToList')
+    path(r'^(?P<pk>)/save$',saveToList,name ='saveToList'),
+path(r'^(?P<pk>)/delete$',deleteFromList,name ='deleteFromList')
 ]
