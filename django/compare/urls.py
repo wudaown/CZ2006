2263007359bc0ff8f44e34e3927fb7f42d69d5d1
Archonsh <archonshen@gmail.com>
2018-04-02 19:29:58 +0800

    Revert "# Conflicts:"
    This reverts commit 234beca57b7bbc45885f1e56c058f4be4648f71c, reversing
    changes made to cb1dd236382083dcac452c598bafc4cccaa5c95f.

--- django/compare/urls.py
+++ django/compare/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import url
-from django.urls import path
 from compare import views
 
 urlpatterns = [
-    path('add/', views.add, name='compare_add'),
-    path('remove/', views.remove, name='compare_remove'),
-    path('remove_all/', views.remove_all, name='compare_remove_all'),
-    path('<order_by>/', views.result, name='compare_result'),
-    # passing in based on what to order by! default is by price
+    #path('add/', views.add, name='compare_add'),
+    #path('remove/', views.remove, name='compare_remove'),
+    #path('<order_by>/', views.result, name='compare_result'),
+    # passing in based on what to order by!
 ]
--- django/compare/views.py
+++ django/compare/views.py
@@ -6,50 +6,24 @@ from schools.models import Kindergarten
 
 
 def add(request):
-	try:
-		u = User.objects.get(username=request.session['member_id'])
-	except:
-		return render(request, 'login.html')
-
-	if len(CompareList.objects.filter(user=u))>3:
-		return render(request, 'compare_modify.html', {'msg':
-		'Cannot add to compare because there are already 3 schools in compare.'})
-
-	try:
-		s = request.POST.get('kindergarten_key') # TODO
+	u = request.POST.get('user_key')
+	s = request.POST.get('kindergarten_key')
+	if type(u) == User and type(s) == Kindergarten and u and s:  # not blank
 		relation = CompareList(user=u, school=s)
 		relation.save()
-		return render(request, 'compare_modify.html', {'msg': 'Add to compare list succeeded.'})
-	except:
-		return render(request, 'compare_modify.html', {'msg': 'Add to compare list failed.'})
+		return render(request, 'compare_modify.html', {'msg': 'add to compare list succeeded.'})
+	else:
+		return render(request, 'compare_modify.html', {'msg': 'add to compare list failed.'})
 
 
 def remove(request):
-	try:
-		u = User.objects.get(username=request.session['member_id'])
-	except:
-		return render(request, 'login.html')
-
-	s = request.POST.get('kindergarten_key')  # TODO
+	u = request.POST.get('user_key')
+	s = request.POST.get('kindergarten_key')
 	CompareList.objects.filter(user=u, school=s).delete()
-
-	return render(request, 'compare_modify.html', {'msg': '%s deleted from %s\'s compare list'.format(s, u)})
-
-def remove_all(request):
-	try:
-		u = User.objects.get(username=request.session['member_id'])
-	except:
-		return render(request, 'login.html')
-
-	CompareList.objects.filter(user=u).delete()
-	return render(request, 'compare_modify.html', {'msg': 'All schools deleted from %s\'s compare list'.format(u)})
+	#return render(request, 'compare_modify.html', {'msg': '%s deleted from %s\'s compare list'.format(s, u))
 
 
-def result(request, order_by='price'):
-	try:
-		u = User.objects.get(username=request.session['member_id'])
-	except:
-		return render(request, 'login.html')
-
+def result(request, order_by='data'):
+	u = request.POST.get('user_key')
 	compareList = CompareList.objects.order_by(order_by).filter(user=u)
 	return render(request, 'compare.html', {'user_list': compareList})
