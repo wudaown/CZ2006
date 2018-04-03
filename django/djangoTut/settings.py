032d85691b3b09d2ed3ad121251f490b1da6c41e
Ahrayz <Ahrayz@users.noreply.github.com>
2018-04-03 18:49:18 +0800

    ,

--- django/djangoTut/settings.py
+++ django/djangoTut/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	'users.apps.UsersConfig',
-	'schools.apps.SchoolsConfig'
+	'schools.apps.SchoolsConfig',
 	'compare',
 ]
 
