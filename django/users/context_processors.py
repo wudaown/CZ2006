d2e6df8f17f1205b47fa85e79bcdaff6428bf74c
Archonsh <archonshen@gmail.com>
2018-04-03 21:15:37 +0800

    add context processor

--- django/users/context_processors.py
+++ django/users/context_processors.py
@@ -1,8 +1,16 @@
 from .models import MailBox,Message_Mailbox,User
 
 def NumOfNotification(request):
+    user = None
+    if 'member_id' in request.session:
+        user = User.objects.get(username=request.session['member_id'])
+        print(user)
+    else:
+        for item in request.session:
+            print(item)
     user = User.objects.get(username=request.session['member_id'])
     if user is not None:
+        print(user)
         mailbox = user.mailbox
         unread_sum = Message_Mailbox.objects.count(mailbox_id=mailbox.id, viewed=False)
         return {'unread': unread_sum}
